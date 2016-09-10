1a8c730697f6a17e316a734af80c1b99d3d4878e
LuisF25 <luisfisherjr@gmail.com>
2016-09-10 09:00:35 -0700

    homework 1 working on matrix

--- src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
+++ src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
@@ -3,7 +3,12 @@ package csula.cs4660.graphs.representations;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Node;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,18 +28,105 @@ public class AdjacencyMatrix implements Representation {
 
     }
 
+    private void readFile(File file) {
+
+        BufferedReader reader;
+        String curLine;
+        int nodeCount;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            // ArrayList<Node<Integer>> nodes = new ArrayList<Node<Integer>>();
+            Node<Integer> tempNode;
+            Edge tempEdge;
+
+            curLine = reader.readLine();
+            nodeCount = Integer.parseInt(curLine);
+
+            nodes = new Node[nodeCount];
+            adjacencyMatrix =  new int[nodeCount][nodeCount];
+
+            for (int[] row : adjacencyMatrix) {
+                Arrays.fill(row, 0);
+            }
+
+            for(int index = 0; index < nodes.length; index++) {
+                nodes[index] = new Node<Integer>(index);
+            }
+
+            while ((curLine = reader.readLine()) != null) {
+
+                String[] sRow = curLine.split(":");
+
+                int length = sRow.length;
+
+                int[] rowValues = new int[length];
+
+                adjacencyMatrix[rowValues[0]][rowValues[1]] = rowValues[2];
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public boolean adjacent(Node x, Node y) {
-        return false;
+
+        int xData = (int)x.getData();
+        int yData = (int)y.getData();
+
+        return ((adjacencyMatrix[xData][yData] != 0) || (adjacencyMatrix[yData][xData] != 0))
     }
 
     @Override
     public List<Node> neighbors(Node x) {
-        return null;
+
+        boolean inArray = false;
+        int positionX = 0;
+
+        for(int index = 0; index < nodes.length; index++) {
+            if (nodes[index].equals(x)) {
+                inArray = true;
+                positionX = index;
+                break;
+            }
+        }
+
+        if (!inArray) return null;
+
+        ArrayList<Node> neighbors = new ArrayList<Node>();
+
+        for(int index = 0; index <  adjacencyMatrix[positionX].length; index++) {
+            if (adjacencyMatrix[positionX][index] > 0) neighbors.add(nodes[index]);
+        }
+        return neighbors;
     }
 
     @Override
     public boolean addNode(Node x) {
+
+        Node[] newNodes = new Node[nodes.length + 1];
+        int[][] newMatrix = new int[nodes.length + 1][nodes.length + 1];
+
+        newNodes[nodes.length] = x;
+
+        for(int index = 0; index < nodes.length; index++) {
+            newNodes[index] = nodes[index];
+        }
+
+
+        for(int i = 0;  i < adjacencyMatrix.length; i++) {
+            for(int j = 0; j < adjacencyMatrix[0].length; j++) {
+
+            }
+        }
+
+
+        nodes = newNodes;
+        adjacencyMatrix = newMatrix;
+
         return false;
     }
 
