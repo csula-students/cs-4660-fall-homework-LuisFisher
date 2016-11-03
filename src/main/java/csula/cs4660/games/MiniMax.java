477ab78585385617144e90e3e987ecb9bf70bd46
LuisF25 <luisfisherjr@gmail.com>
2016-11-03 16:25:04 -0700

    minmax completed

--- src/main/java/csula/cs4660/games/MiniMax.java
+++ src/main/java/csula/cs4660/games/MiniMax.java
@@ -7,8 +7,23 @@ import csula.cs4660.graphs.Node;
 import java.util.List;
 
 public class MiniMax {
-
+    
     public static Node getBestMove(Graph graph, Node root, Integer depth, Boolean maximizingPlayer) {
+
+        miniMax(graph, root, depth, maximizingPlayer);
+
+        int rootValue = ((MiniMaxState)root.getData()).getValue();
+
+        for (Node node: graph.neighbors(root)) {
+
+            int nodeValue = ((MiniMaxState)node.getData()).getValue();
+            if (rootValue == nodeValue) return  node;
+        }
+
+        return root;
+    }
+
+    private static Node miniMax(Graph graph, Node root, Integer depth, Boolean maximizingPlayer) {
 
         // TODO: implement minimax to retrieve best move
         // NOTE: you should mutate graph and node as you traverse and update value
@@ -26,20 +41,8 @@ public class MiniMax {
         if (maximizingPlayer) {
             int bestValue = Integer.MIN_VALUE; // negative infinite
             for (Node node: children) {
-
-                MiniMaxState nodeMMS = (MiniMaxState)node.getData();
-
-                int value = ((MiniMaxState)getBestMove(graph, node, depth - 1, false).getData()).getValue();
-
-                if (bestValue <= value) {
-                    rootMMS.setIndex(nodeMMS.getIndex());
-                }
-
+                int value = ((MiniMaxState) miniMax(graph, node, depth - 1, false).getData()).getValue();
                 bestValue = Math.max(bestValue, value);
-
-
-
-
                 rootMMS.setValue(bestValue);
             }
             return root;
@@ -48,51 +51,11 @@ public class MiniMax {
         else {
             int bestValue = Integer.MAX_VALUE; // positive infinite
             for (Node node: children) {
-
-                MiniMaxState nodeMMS = (MiniMaxState)node.getData();
-
-                int value = ((MiniMaxState)getBestMove(graph, node, depth - 1, true).getData()).getValue();
-
-                if (bestValue <= value) {
-                    rootMMS.setIndex(nodeMMS.getIndex());
-                }
-
-
+                int value = ((MiniMaxState) miniMax(graph, node, depth - 1, true).getData()).getValue();
                 bestValue = Math.min(bestValue, value);
-
                 rootMMS.setValue(bestValue);
-
             }
             return root;
         }
     }
 }
-
-/*
-function minimax(graph, sourceNode, depth, maximizingPlayer) {
-    // usually being optimized in a way to compute even before
-    // the end of game by **evaluate** function
-    if (depth = 0 || sourceNode is a leaf) {
-        return evaluate(soureNode.gameState); // return a number
-    }
-
-    if (maximizingPlayer) {
-        bestValue = Number.MAX_VALUE; // positive infinite
-        for (node in graph.neighbors(sourceNode)) {
-            value = minimax(node, graph, depth - 1, false);
-            bestValue = Math.max(bestValue, value);
-        }
-        return bestValue;
-    } else {
-        bestValue = Number.MIN_VALUE; // negative infinite
-        for (node in graph.neighbors(sourceNode)) {
-            value = minimax(node, graph, depth - 1, true);
-            bestValue = Math.min(bestValue, value);
-        }
-        return bestValue;
-    }
-}
-
-// initial call of the above function to traverse 3 level for example
-minimax(graph, startingNode, 3, TRUE);
- */
